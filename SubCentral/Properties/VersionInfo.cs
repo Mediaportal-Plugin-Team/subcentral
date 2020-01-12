979881dff33e9e36f8d021bf1ae0387fc4d70ea5
doskabouter <paul.doskabouter@gmail.com>
2020-01-12 16:58:58 +0100

    release 1.2.6

--- SubCentral/Properties/VersionInfo.cs
+++ SubCentral/Properties/VersionInfo.cs
@@ -1,2 +1,2 @@
-﻿[assembly: System.Reflection.AssemblyVersion("1.2.5")]
-[assembly: System.Reflection.AssemblyFileVersion("1.2.5")]
+﻿[assembly: System.Reflection.AssemblyVersion("1.2.6")]
+[assembly: System.Reflection.AssemblyFileVersion("1.2.6")]
